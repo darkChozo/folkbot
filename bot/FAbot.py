1381d89b3b077a0e1e6ad7320839c5c33a7d6633
Mark Dennehy <mark.dennehy@workday.com>
2015-11-15 02:43:34 +0000

    Supervise logging tweaks

--- bot/FAbot.py
+++ bot/FAbot.py
@@ -33,7 +33,7 @@ class FAbot(object):
         self.botMethods = ['start', 'stop']
 
         # Logging
-        logging.basicConfig(filename="logs/FA_bot.log", level=logging.DEBUG,
+        logging.basicConfig(filename="log/FA_bot.log", level=logging.DEBUG,
                             format="%(asctime)-15s %(message)s")
         logging.info("FAbot starting up")
         logging.info("Registering commands: ")
